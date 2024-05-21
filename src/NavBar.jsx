cc9cbcf905d5c8c72aefde6adf4d20b080dbfe55
Yukun Zhang <yukun@yukuns-mbp.mynetworksettings.com>
2024-05-21 00:24:34 -0400

    block28 complete

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,27 +1,14 @@
 import { useNavigate } from 'react-router-dom'
 
 const Navigation = () => {
+
     const navigate = useNavigate()
-   
-
-    const goToHomePage = () =>{
-        navigate('/');
-    }
-
-    const goToBluePage = () =>{
-        navigate('/blue');
-    }
-
-    const goToRedPage = () =>{
-        navigate('/red');
-    } 
-    
 
     return (
         <div id="navbar">
-            <button onClick={goToHomePage}>Home</button>
-            <button onClick={goToBluePage}>Blue</button>
-            <button onClick={goToRedPage}>Red</button>
+            <button onClick={()=> navigate('/')}>Home</button>
+            <button onClick={()=> navigate('/blue')}>Blue</button>
+            <button onClick={()=> navigate('/red')}>Red</button>
         </div>
     )
 }
